3380eee5e1c1dccdb239719f53b81055549c5815
Tommi Carleman <me@tommcarleman.net>
2013-01-13 16:11:14 +0100

    remove jshint added jslint

--- install.sh
+++ install.sh
@@ -11,7 +11,6 @@ rm ~/.gvimrc
 rm ~/.gitconfig
 rm ~/.gitignore
 rm ~/.tmux.conf
-rm ~/.bash_it
 
 echo "Symlinking files"
 ln -s ~/Github/dotfiles/vimrc ~/.vimrc
@@ -20,7 +19,6 @@ ln -s ~/Github/dotfiles/gvimrc ~/.gvimrc
 ln -s ~/Github/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Github/dotfiles/gitignore ~/.gitignore
 ln -s ~/Github/dotfiles/tmux ~/.tmux.conf
-ln -s ~/Github/dotfiles/bash-it ~/.bash_it
 
 echo "Updating submodules"
 git submodule foreach git pull origin master #--recurse-submodules
